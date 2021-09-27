50fd83186a4e41323902d22e6c3884aeda60fa2a
John Murray <69658747+lovendatj@users.noreply.github.com>
2021-09-26 22:09:29 -0400

    typing changes

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from Taps.GoogleRSS import GoogleRSS
 
 with GoogleRSS(verbose=True, outpath='./output') as goog:
-    results = goog.get_xml(query=['APPL new phone','GME stock'], when='24h', dump_file=True)
+    results = goog.get_xml(
+        query=['APPL new phone', 'GME stock'], when='24h', dump_file=True)
