6f98590c55f89d8f948cdbf1765716f8e6f95b11
John Murray <69658747+lovendatj@users.noreply.github.com>
2021-09-30 10:25:53 -0400

    edits to readme and requirements.txt

--- test.py
+++ test.py
@@ -2,16 +2,19 @@ from Taps.GoogleRSS.GoogleRSS import GoogleRSS
 from Taps.RedditScraping.RedditData import RedditData
 
 
+# Data output path
 opath = '../Taps/GoogleRSS/output'
 with GoogleRSS(out_path=opath) as goog:
     results = goog.get_xml(
         query=['APPL new phone', 'GME stock'], when='24h')
     goog.file_out(results)
 
+# Data output path
 opath = '../Taps/RedditScraping/output'
+# Any subreddit
 sub = ['WallStreetbets']
+# The queue parameter takes options 'submissions' or comments'
 queue = 'comments'
 with RedditData(sub=sub, queue=queue, out_path=opath) as reddit:
     results = reddit.data_dump()
-    print(results)
     reddit.file_out(results)
